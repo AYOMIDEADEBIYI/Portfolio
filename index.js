80585e3381ee861a1091bdc8842e0cf787eff2b0
Uniquedev / <135809277+AYOMIDEADEBIYI@users.noreply.github.com>
2024-03-27 06:37:03 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+function myHeader() {
+    let header = document.getElementById("header");
+    let lol = document.getElementById("lol");
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 0) {
+            header.classList.add("active");
+            lol.src = "pic/bulb2.png";
+        } else {
+            header.classList.remove("active");
+            lol.src = "pic/bulb.png";
+        }
+
+    })
+}
+myHeader();
+
+function myBars() {
+    let bars = document.querySelector("#bar");
+    let nav = document.querySelector(".navigation");
+    bars.onclick = function () {
+        if (nav.style.right == "0%") {
+            nav.style.right = "-50%";
+            bars.src = "img/menu.png"
+        } else {
+            nav.style.right = "0%";
+            bars.src = "img/x.png"
+        }
+        nav.classList.toggle("new")
+    }
+
+}
+myBars()
+
+function toTop() {
+    let top = document.querySelector(".top");
+    window.onscroll = () => {
+        if (window.scrollY > 100 || document.documentElement.scrollTop > 100) {
+            top.style.display = "block"
+        } else {
+            top.style.display = "none"
+        }
+    }
+
+    top.onclick = () => {
+        scrollTo(0, 0)
+    }
+
+}
+toTop();
